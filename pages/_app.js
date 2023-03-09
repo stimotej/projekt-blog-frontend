bd5ac6162d2092f4b46cc62e82005bc068be3df8
Timotej Sofijanović <76438562+stimotej@users.noreply.github.com>
2023-03-09 15:58:52 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:5000/api";
-axios.defaults.baseURL = "https://projekt-blog-backend.herokuapp.com/api";
+axios.defaults.baseURL = "https://projekt-blog-backend.onrender.com/api";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
